7538220620516fb28ab5982a635dd36e5986e7c1
Eric McDonald <librick@users.noreply.github.com>
2023-07-20 11:02:16 -0400

    simplify flatpaks

--- scripts/install-flatpaks.sh
+++ scripts/install-flatpaks.sh
@@ -11,17 +11,17 @@ DEBIAN_FRONTEND=noninteractive
 
 # Install flatpaks for system
 apt-get install -y flatpak
-flatpak remote-add -y --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak remote-add -y --if-not-exists gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
-flatpak install gnome-nightly org.gnome.Builder.Devel
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
+flatpak install -y gnome-nightly org.gnome.Builder.Devel
 readarray -t fps_system < $plaintext_dir/flatpaks-system.txt
 for fp in "${fps_system[@]}"; do
     flatpak install flathub $fp
 done
 
-# Install flatpaks for user
-sudo -u $username flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-readarray -t fps_user < $plaintext_dir/flatpaks-user.txt
-for fp in "${fps_user[@]}"; do
-    sudo -u $username flatpak --user install flathub $fp
-done
+# # Install flatpaks for user
+# sudo -u $username flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# readarray -t fps_user < $plaintext_dir/flatpaks-user.txt
+# for fp in "${fps_user[@]}"; do
+#     sudo -u $username flatpak --user install flathub $fp
+# done
